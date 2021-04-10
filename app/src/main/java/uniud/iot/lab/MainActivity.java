2abc956c1daee19f1e0e57d4f81fac0453e080e7
Emanuele <emanuelelena021099@gmail.com>
2021-04-10 17:36:18 +0200

    git ignore for Android

---
+++ app/src/main/java/uniud/iot/lab/MainActivity.java
@@ -0,0 +1,19 @@
+package uniud.iot.lab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.EventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
