68da8f57b4aea5da265374c5b1a48a4f7021c6ed
Alex Dumitru <aaaalexdumitru@gmail.com>
2021-04-14 22:02:09 +0200

    Begin implementation of FakeDistanceRequester

--- app/src/main/java/uniud/iot/lab/dataProvider/update/requester/FakeDistanceRequester.java
+++ app/src/main/java/uniud/iot/lab/dataProvider/update/requester/FakeDistanceRequester.java
@@ -1,8 +1,28 @@
 package uniud.iot.lab.dataProvider.update.requester;
+import org.json.JSONObject;
+
 
 public class FakeDistanceRequester implements Requester{
-    @Override
-    public void request() {
+    private String address = null;
+    private Integer port = null;
+    private JSONObject data = null;
 
+
+    public FakeDistanceRequester(String address, Integer port){
+        this.address = address;
+        this.port = port;
     }
+
+    @Override
+    public void request(){
+    }
+
+
+    public JSONObject response(){
+        return this.data;
+    }
+
+
 }
+
+
--- app/src/main/java/uniud/iot/lab/dataProvider/update/requester/RequesterFactory.java
+++ app/src/main/java/uniud/iot/lab/dataProvider/update/requester/RequesterFactory.java
@@ -1,11 +1,13 @@
 package uniud.iot.lab.dataProvider.update.requester;
 
+
 public class RequesterFactory {
-    public static Requester getFakeDistanceRequester(){
-        return null;
+    public static Requester getFakeDistanceRequester(String address, Integer port){
+        return new FakeDistanceRequester(address,port);
     }
 
-    public static Requester getFakeVideoRequester(){
+    public static Requester getFakeVideoRequester(String address, Integer port){
+
         return null;
     }
 }
