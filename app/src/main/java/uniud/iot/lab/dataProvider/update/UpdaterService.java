774d07a7c492c7a97ef10d519db79f252fa501a3
Alex Dumitru <aaaalexdumitru@gmail.com>
2021-04-15 21:14:17 +0200

    Created method injection DataProvider

--- app/src/main/java/uniud/iot/lab/dataProvider/update/UpdaterService.java
+++ app/src/main/java/uniud/iot/lab/dataProvider/update/UpdaterService.java
@@ -1,7 +1,9 @@
 package uniud.iot.lab.dataProvider.update;
+import java.util.Observable;
+
 import uniud.iot.lab.dataProvider.update.updater.Updater;
 
 
 public interface UpdaterService {
-    public Updater createUpdater();
+    public Updater createUpdater(Observable dataProvider);
 }
